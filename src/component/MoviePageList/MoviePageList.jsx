291843b37bec51e831943b99567f377faacb9296
forysiak-vova <volodja.forus@gmail.com>
2022-02-24 01:25:55 +0100

    detail

---
+++ src/component/MoviePageList/MoviePageList.jsx
@@ -0,0 +1,38 @@
+import defaultImg from '../../images/noImageAvailable.jpg'
+
+const MoviePageList = ({ movi }) => {
+//  console.log(movi.genres)
+   
+   return <ul> 
+      <li>{
+         <img src={
+          movi.poster_path ?
+               `https://image.tmdb.org/t/p/w500${movi.poster_path}` :
+               defaultImg
+         } alt="" />
+      }</li>
+      <li>
+         {movi.title}
+
+         ({movi.release_date.substring(0, 4)})
+         <p>
+            
+        use Score: {movi.vote_average * 10}%
+      </p>
+      </li>
+      <li>
+         <h3>Overview</h3>
+        
+         {movi.overview}
+      </li>
+      <li>
+         <h3>Genres</h3>
+     
+         {movi.genres.map(({id,name}) => <span key={id}>{name}</span>)}
+       </li>
+   </ul>
+   
+  
+}
+
+export default MoviePageList
--- src/view/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/view/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,25 +1,26 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import MoviePageList from '../../component/MoviePageList'
 
 const MovieDetailsPage = () => {
    // console.log(useParams())
    const { movieId } = useParams()
-   console.log(movieId)
-   const [movi, setMovi] = useState([]);
+   // console.log(movieId)
+   const [movi, setMovi] = useState('');
    useEffect(() => {
-      if (!movi) {
-         return
-      }
+      
+     
       fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=0754829cbe2d4a3d2043b315bf2671de&language=en-US`)
          .then(response => response.json())
-         .then(data => {setMovi(prev => [...prev, data])})
-      // .then(console.log)
+         .then(data => setMovi(data))
+      
    },[])
    console.log(movi)
    return (
       <>
-      
-     {movieId}
+         {movi && <MoviePageList movi={movi}/>}
+         
+     {/* {movieId} */}
       </>
       
    );
